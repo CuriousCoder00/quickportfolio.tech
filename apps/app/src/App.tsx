60b638c06ef71782ba979a19cb5ab424d798c861
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-10 15:45:20 +0530

    feat: initialize Vite + React app structure with TypeScript and Tailwind CSS

---
+++ apps/app/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@repo/ui/components/ui/button";
+import { Input } from "@repo/ui/components/ui/input";
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center bg-black text-white">
+      QuickPortfolio
+      <Button>Get Started</Button>
+      <Input type="text" placeholder="Enter your email" />
+    </div>
+  );
+}
+
+export default App;
