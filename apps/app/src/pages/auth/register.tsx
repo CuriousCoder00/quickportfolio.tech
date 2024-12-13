2e4c6323efe085e28ef0244b23f5d77978b370cf
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-13 13:29:47 +0530

    feat: add signup form component and routing for user registration

---
+++ apps/app/src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import SignupForm from "../../components/auth/signup-form";
+
+const SignupPage = () => {
+  return (
+    <div className="w-screen min-h-dvh flex items-center justify-center">
+      <SignupForm />
+    </div>
+  );
+};
+
+export default SignupPage;
