4233eb9a3e68292ea7f09caf5850fad30834feeb
Kapil Jangid <kapiljangid040@gmail.com>
2024-12-23 20:24:08 +0530

    feat: adjust padding on HomePage for improved layout on larger screens

--- apps/app/src/pages/home.tsx
+++ apps/app/src/pages/home.tsx
@@ -2,7 +2,7 @@ import Hero from "../components/landing/hero";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col no-scrollbar">
+    <div className="flex flex-col no-scrollbar md:pt-40 pt-20">
       <Hero />
     </div>
   );
